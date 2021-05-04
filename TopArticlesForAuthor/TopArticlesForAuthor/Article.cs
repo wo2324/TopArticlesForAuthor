78b3fa33c9defd912a3da80ce8bd06498b460975
Wojciech Default <wojciechklanowski@gmail.com>
2021-05-04 22:32:58 +0200

    Add Article class

---
+++ TopArticlesForAuthor/TopArticlesForAuthor/Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopArticlesForAuthor
+{
+    class Article
+    {
+        internal string title;
+        internal string url;
+        internal string author;
+        internal string num_comments;
+        internal string story_id;
+        internal string story_title;
+        internal string story_url;
+        internal string parent_id;
+        internal string created_at;
+    }
+}
