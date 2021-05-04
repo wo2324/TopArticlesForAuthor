6b88bdbe2c0e2b767f698d56a34c0e956faca75a
Wojciech Default <wojciechklanowski@gmail.com>
2021-05-04 22:21:35 +0200

    Gain API response

--- TopArticlesForAuthor/TopArticlesForAuthor/Program.cs
+++ TopArticlesForAuthor/TopArticlesForAuthor/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,14 +17,30 @@ namespace TopArticlesForAuthor
         {
             string userName = "olalonde";
             int limit = 1;
-            topArticles(userName, limit);
+            topArticles(GetSpecification(userName, limit));
             Console.ReadLine();
         }
 
-        static List<string> topArticles(string userName, int limit)
+        static List<string> topArticles(string specification)
         {
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Content-Type", "application/json");
+            request.Resource = $"articles{specification}";
+            var response = client.Execute(request);
+            if (!response.IsSuccessful || response.StatusCode != HttpStatusCode.OK)
+            {
+                throw new Exception(response.Content);
+            }
+
             List<string> Articles = new List<string>();
             return Articles;
+        }
+
+        static string GetSpecification(string userName, int limit)
+        {
+            return $"?" +
+                $"author={userName}&" +
+                $"page={limit}";
         }
     }
 }
