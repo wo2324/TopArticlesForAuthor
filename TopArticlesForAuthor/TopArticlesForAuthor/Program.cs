640eadad8f7dbd36bff5e9b3b05eb24274483f30
Wojciech Default <wojciechklanowski@gmail.com>
2021-05-05 00:21:18 +0200

    Pseudocode

--- TopArticlesForAuthor/TopArticlesForAuthor/Program.cs
+++ TopArticlesForAuthor/TopArticlesForAuthor/Program.cs
@@ -61,11 +61,14 @@ namespace TopArticlesForAuthor
             try
             {
                 List<Article> Articles = GetArticles(GetSpecification(userName, limit));
+                //definiwanie name
                 /* 1. Top limit
                  * 2. if (story == null && story title == null) -> ignore
+                 * 2. *
                  * 3. name = title == null ? title : story_title
                  * 4. order by num_comments desc, name asc
                  */
+                //var results = Articles.OrderBy(x => x.num_comments).ThenByDescending(x => x.).Select(x => x.Grp);
                 return null;
             }
             catch (Exception exception)
